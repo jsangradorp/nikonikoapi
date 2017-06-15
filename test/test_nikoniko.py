b9af3a75fd5a73c54bc10afae8f983bd787f177d
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-06-15 23:02:41 +0200

    Test (and fail) that Andres == Julio

--- test/test_nikoniko.py
+++ test/test_nikoniko.py
@@ -1,22 +1,31 @@
 import logging
-from entities import Person
-from nikoniko.nikoniko import person
+from entities import Session, Person
+from nikoniko import person
+import nikoniko
 import hug
 
 
 logger = logging.getLogger(__name__)
 
 
-def test_person_returns_a_person():
-    # Given
-    # When
-    person1 = Person("Julio")
-    person2 = Person("Julio")
-    # Then
-    assert person1.label == person2.label
+class TestAPI(object):
+    def test_person_returns_a_person(self):
+        # Given
+        # When
+        person1 = Person("Julio")
+        person2 = Person("Julio")
+        # Then
+        assert person1.label == person2.label
 
-
-def test_hug_tests():
-    response = hug.test.get(person, 'happy_birthday', {'name': 'Timothy', 'age': 25})  # Returns a Response object
-    logger.info(response)
-    assert(response is None)
+    def test_hug_tests(self):
+        # Given
+        person = Person("Andres")
+        person.id = 25
+        session = Session()
+        session.add(person)
+        session.flush()
+        session.commit()
+        # import pdb; pdb.set_trace()
+        response = hug.test.get(nikoniko, '/person', {'id': 25})  # Returns a Response object
+        logger.info(response)
+        assert(response.data == "Julio")
