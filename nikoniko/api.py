90c2d30f58540bc42a15ea22e4bc9cf66a1d9ad2
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-07-15 16:27:21 +0200

    Authenticate with fake admin/root credentials

--- nikoniko/api.py
+++ nikoniko/api.py
@@ -2,6 +2,7 @@ import logging
 import datetime
 
 import hug
+import jwt
 from sqlalchemy.orm import aliased
 from falcon import HTTP_404
 
@@ -18,22 +19,63 @@ session = Session()
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api))
 
+one_person = Person(id=1, label='Administrator')
+session.add(one_person)
+session.commit()
 
-@hug.get('/people/{id}')
-def person(id: hug.types.number):
+
+@hug.post('/login')
+def login(username: hug.types.text, password: hug.types.text):
+    '''Authenticate and return a token'''
+    secret_key = 'super-secret-key-please-change'
+    mockusername = 'admin'
+    mockpassword = 'root'
+    if mockpassword == password and mockusername == username: # This is an example. Don't do that.
+        created = datetime.datetime.now()
+        return {
+            'user': 1,
+            'token' : jwt.encode(
+                {
+                    'user': 1,
+                    'created': created.isoformat(),
+                    'exp': (created + datetime.timedelta(days=1)).timestamp()
+                },
+                secret_key,
+                algorithm='HS256'
+            )}
+    return 'Invalid username and/or password for user: {0}'.format(username)
+
+
+def token_verify(token):
+    secret_key = 'super-secret-key-please-change'
+    try:
+        return jwt.decode(token, secret_key, algorithm='HS256')
+    except jwt.DecodeError:
+        return False
+
+
+token_key_authentication = hug.authentication.token(token_verify)
+
+
+@hug.get('/people/{id}', requires=token_key_authentication)
+def person(id: hug.types.number, response, user: hug.directives.user):
     '''Returns a person'''
-    res = session.query(Person).filter_by(id=id).one()
+    try:
+        res = session.query(Person).filter_by(id=id).one()
+    except:
+        response.status = HTTP_404
+        return None
     return person_schema.dump(res).data
 
 
-@hug.get('/people')
+@hug.get('/people', requires=token_key_authentication)
 def people():
     '''Returns all the people'''
     res = session.query(Person).all()
     return people_schema.dump(res).data
 
 
-@hug.get('/boards/{id}')
+@hug.get('/boards/{id}', requires=token_key_authentication)
 def board(id: hug.types.number, response):
     '''Returns a board'''
     try:
@@ -44,14 +86,16 @@ def board(id: hug.types.number, response):
     return board_schema.dump(res).data
 
 
-@hug.get('/boards')
+@hug.get('/boards', requires=token_key_authentication)
 def boards():
     '''Returns all boards'''
     res = session.query(Board).all()
     return boards_schema.dump(res).data
 
 
-@hug.get('/reportedfeelings/boards/{board_id}/people/{person_id}/date/{date}')
+@hug.get(
+        '/reportedfeelings/boards/{board_id}/people/{person_id}/date/{date}',
+        requires=token_key_authentication)
 def get_reportedFeeling(
         board_id: hug.types.number,
         person_id: hug.types.number,
@@ -59,14 +103,20 @@ def get_reportedFeeling(
         response):
     '''Returns a specific reported feeling for a board, person and date'''
     try:
-        res = session.query(ReportedFeeling).filter_by(board_id=board_id, person_id=person_id, date=date).one()
+        res = session.query(ReportedFeeling).filter_by(
+                        board_id=board_id,
+                        person_id=person_id,
+                        date=date
+                        ).one()
     except:
         response.status = HTTP_404
         return None
     return reportedfeeling_schema.dump(res).data
 
 
-@hug.post('/reportedfeelings/boards/{board_id}/people/{person_id}/date/{date}')
+@hug.post(
+        '/reportedfeelings/boards/{board_id}/people/{person_id}/date/{date}',
+        requires=token_key_authentication)
 def create_reportedFeeling(
         board_id: hug.types.number,
         person_id: hug.types.number,
@@ -74,10 +124,22 @@ def create_reportedFeeling(
         date: hug.types.text):
     '''Creates a new reportedFeeling'''
     try:
-        reportedFeeling = session.query(ReportedFeeling).filter_by(board_id=board_id, person_id=person_id, date=date).one()
+        reportedFeeling = session.query(ReportedFeeling).filter_by(
+                                    board_id=board_id,
+                                    person_id=person_id,
+                                    date=date
+                                    ).one()
         reportedFeeling.feeling = feeling
     except:
-        reportedFeeling = ReportedFeeling(person_id=person_id, board_id=board_id, date=datetime.datetime.strptime(date, "%Y-%m-%d").date(), feeling=feeling)
+        reportedFeeling = ReportedFeeling(
+                            person_id=person_id,
+                            board_id=board_id,
+                            date=datetime.datetime.strptime(
+                                date,
+                                "%Y-%m-%d"
+                                ).date(),
+                            feeling=feeling
+                            )
         session.add(reportedFeeling)
     session.commit()
     return reportedfeeling_schema.dump(reportedFeeling).data
