bed37e60675cc00d5bcbaec83aa69b2e8f769361
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-07-15 21:04:02 +0200

    Activate virtualenv on run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
+if [ -z "$VIRTUAL_ENV" ] ; then
+    . .venv/bin/activate
+fi
 uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads --http 0.0.0.0:8000 --wsgi-file nikoniko/api.py --callable __hug_wsgi__
