a9bcf0892d6b9697ec0a4c309ef8e2333ff62bf7
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-07-15 17:19:05 +0200

    Use postgres instead of in-memory sqlite

---
+++ scripts/db-setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo This script will destroy and create the nikoniko user and database.
+echo This will ERASE ALL YOUR DATA.
+echo Edit and run it if you are sure you want to do that.
+exit
+cat << EOF | psql postgres
+drop database if exists nikoniko;
+drop user if exists nikoniko;
+create user nikoniko with login createdb unencrypted password 'awesomepassword';
+create database nikoniko with owner = nikoniko;
+EOF
+
