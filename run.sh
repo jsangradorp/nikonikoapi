49f15da1c5ade369d7dc8294f3ffcda343486bda
Julio Sangrador-Paton <julio.sangrador@schibsted.com>
2017-08-13 21:15:26 +0200

    Use nginx to provide SSL

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 if [ -z "$VIRTUAL_ENV" ] ; then
     . .venv/bin/activate
 fi
-export JWT_SECRET_KEY='awesome-secret-key'
-uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads --http 0.0.0.0:8000 --wsgi-file nikoniko/api.py --callable __hug_wsgi__
+nginx
+uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads --socket /tmp/uwsgi.sock --wsgi-file nikoniko/api.py --callable __hug_wsgi__
