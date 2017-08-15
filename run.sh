2b42144a678607291d713ad2dac3d32182a41e02
Julio Sangrador-Paton <julio.sangrador@schibsted.com>
2017-08-15 15:56:00 +0200

    Use global nginx config

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 if [ -z "$VIRTUAL_ENV" ] ; then
     . .venv/bin/activate
 fi
-nginx
-uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads --socket /tmp/uwsgi.sock --wsgi-file nikoniko/api.py --callable __hug_wsgi__
+uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads --socket /tmp/uwsgi.sock --chmod-socket --wsgi-file nikoniko/api.py --callable __hug_wsgi__
