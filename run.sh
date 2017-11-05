9f9d1f4a41385c5a4f0bbcaae38ff2ef93f1ef13
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-11-05 01:52:55 +0100

    Add local mode

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env sh
+
+# DO_BOOTSTRAP_DB=true JWT_SECRET_KEY=pp LOCAL=y ./run.sh
+
 if [ -z "$VIRTUAL_ENV" ] ; then
     . .venv/bin/activate
 fi
-uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads --socket /tmp/uwsgi.sock --chmod-socket --wsgi-file nikoniko/api.py --callable __hug_wsgi__
+
+trap "kill_children" 0
+kill_children() {
+        echo
+            [ -n "$proxypid"  ] && kill -9 $proxypid 2> /dev/null
+
+}
+
+if [ "$LOCAL" != "y" -a "$LOCAL" != "1" -a "$LOCAL" != "t" ] ; then
+    SOCKET_OPTS="--socket /tmp/uwsgi.sock --chmod-socket"
+else
+    SOCKET_OPTS="--http :8080"
+    local-ssl-proxy --source 8443 --target 8080 & proxypid=$!
+fi
+uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads $SOCKET_OPTS --wsgi-file nikoniko/api.py --callable __hug_wsgi__
