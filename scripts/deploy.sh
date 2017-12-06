aef40161a4ad1eaa9187201777647800ac6dc8a3
Julio Sangrador-Paton <julio.sangrador@schibsted.com>
2017-12-06 22:40:01 +0100

    Script to deploy API using cdist

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,10 @@
 DEST=$1
 [ -z "$DEST" ] && DEST=192.168.69.185
 
+set -x
+
 cd $(dirname $0)/..
-cdist config -c conf/cdist -vv $DEST
+rm -f dist/*
+python setup.py bdist_wheel
+cksum dist/nikoniko*.whl > dist/checksum
+cdist config -c conf/cdist -vvv $DEST
