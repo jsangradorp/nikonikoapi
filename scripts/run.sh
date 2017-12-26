103f2582b9d42c6a880e5ae40bd371e5d3d0ab61
Julio Sangrador-Paton <julio.sangrador@schibsted.com>
2017-12-26 18:55:25 +0100

    Fix path for local run

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,7 +19,7 @@ if [ "$LOCAL" != "y" -a "$LOCAL" != "1" -a "$LOCAL" != "t" ] ; then
     SOCKET_OPTS="--socket /tmp/uwsgi.sock --chmod-socket"
 else
     SOCKET_OPTS="--http :8080"
-    local-ssl-proxy --source 8443 --target 8080 --cert conf/nginx/localhost.crt --key conf/nginx/localhost.key & proxypid=$!
+    local-ssl-proxy --source 8443 --target 8080 --cert conf/etc/nginx/localhost.crt --key conf/etc/nginx/localhost.key & proxypid=$!
 fi
 uwsgi --pythonpath ./.venv/lib/python3.6/site-packages --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' --enable-threads $SOCKET_OPTS --wsgi-file nikoniko/api.py --callable __hug_wsgi__
 
